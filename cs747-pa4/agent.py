052eee6417dcac2deead6757afc8bc95a3b39120
krishnaw14 <krishnaw14@gmail.com>
2019-11-08 22:27:21 +0530

    Refinements

--- cs747-pa4/agent.py
+++ cs747-pa4/agent.py
@@ -8,7 +8,8 @@ class SarsaAgent():
 		self.env = env
 
 		self.alpha = alpha
-		self.epsilon = epsilon
+		self.epsilon_initial = epsilon
+		self.epsilon  = epsilon
 
 		self.Q_values = np.zeros((self.env.num_rows, self.env.num_columns, self.env.num_actions))
 
@@ -49,25 +50,36 @@ class SarsaAgent():
 
 		num_episodes = 180
 		final_time_step_values, final_episode_values = np.zeros((num_episodes,)), np.zeros((num_episodes,))
+		minimum_step_values = []
 
 		for seed in range(num_seed_runs):
-			np.random.seed(seed+4)
-			time_step_values, episode_values = [], []
+			np.random.seed(seed)
+			time_step_values, episode_values,  = [], []
 			time_steps = 0
 			self.Q_values = np.zeros((self.env.num_rows, self.env.num_columns, self.env.num_actions))
+			self.epsilon = self.epsilon_initial
+
 			for i in range(num_episodes):
 				time_step = self.play_episode()
 
 				time_steps += time_step
 				time_step_values.append(time_steps)
 				episode_values.append(i)
+				self.epsilon = self.epsilon_initial/(i+1)
+
+			min_step = np.min(np.array(time_step_values[1:]) - np.array(time_step_values[0:-1]))
+			minimum_step_values.append(min_step)
 			final_time_step_values += np.array(time_step_values)
 			final_episode_values += np.array(episode_values)
 
-		# import pdb; pdb.set_trace()
+		min_step = np.mean(minimum_step_values)
+		cumulative_step = final_time_step_values[-1]/num_seed_runs
+		print('Average Minimum steps to reach the target over {} seed runs of {} episodes: {}'.format(num_seed_runs, 
+			num_episodes, min_step))
+		print('Average Cumulative steps to reach the target over {} seed runs of {} episodes: {}'.format(num_seed_runs, 
+			num_episodes, cumulative_step))
+		plt.plot(final_time_step_values/num_seed_runs, np.int32(final_episode_values/num_seed_runs), label=self.save_plot_path)
 
-
-		plt.plot(np.int32(final_time_step_values/num_seed_runs), np.int32(final_episode_values/num_seed_runs), label=self.save_plot_path)
 		plt.xlabel('Time Steps')
 		plt.ylabel('Episodes')
 		plt.title('Episodes vs Time Steps')
--- cs747-pa4/main.py
+++ cs747-pa4/main.py
@@ -1,22 +1,47 @@
 from agent import SarsaAgent
-import numpy as np 
-import matplotlib.pyplot as plt
-import os
 from environment import WindyGridWorld, WindyGridWorldwithKingMoves, StochasticWindyGridWorld
 
-os.makedirs('plots', exist_ok=True)
+import numpy as np 
+import argparse
+import matplotlib.pyplot as plt
+import os
 
-env = WindyGridWorld()
-agent = SarsaAgent(env, alpha=0.5, epsilon=0.1, save_plot_path='baseline')
-agent.learn(num_seed_runs = 10)
 
-env = WindyGridWorldwithKingMoves()
-agent = SarsaAgent(env, alpha=0.5, epsilon=0.2, save_plot_path='kingmoves')
-agent.learn(num_seed_runs = 10)
+def get_args():
+	parser = argparse.ArgumentParser(description='Markov Decision Problem')
+	parser.add_argument('--baseline', action='store_true', default=False,
+		help='Solving for BaseLine WindyGridWorld Environment')
+	parser.add_argument('--kingmoves', action='store_true', default=False,
+		help='Solving for WindyGridWorld Environment with King Moves')
+	parser.add_argument('--stochastic', action='store_true', default=False,
+		help='Solving for Stochastic WindyGridWorld Environment with King Moves')
+	parser.add_argument('--num_seed_runs', type=int, default=10, 
+		help='Number of seeds to average the learnt values')
+	args = parser.parse_args()
+	return args
 
-# env = StochasticWindyGridWorld()
-# agent = SarsaAgent(env, alpha=0.5, epsilon=0.2, save_plot_path='stochastic')
-# agent.learn(num_seed_runs = 10)
+if __name__ == '__main__':
+	os.makedirs('plots', exist_ok=True)
 
-plt.legend()
-plt.show()
+	args = get_args()
+
+	if not args.baseline and not args.kingmoves and not args.stochastic:
+		print('Please pass the correct argument flag')
+
+	if args.baseline:
+		print('----------------- BaseLine -----------------')
+		env = WindyGridWorld()
+		agent = SarsaAgent(env, alpha=0.5, epsilon=0.1, save_plot_path='plots/baseline.png')
+		agent.learn(num_seed_runs = args.num_seed_runs)
+
+	if args.kingmoves:
+		print('----------------- KingMoves -----------------')
+		env = WindyGridWorldwithKingMoves()
+		agent = SarsaAgent(env, alpha=0.5, epsilon=0.1, save_plot_path='plots/kingmoves.png')
+		agent.learn(num_seed_runs = args.num_seed_runs)
+
+	if args.stochastic:
+		print('----------------- Stocastic with KingMoves -----------------')
+		env = StochasticWindyGridWorld()
+		agent = SarsaAgent(env, alpha=0.5, epsilon=0.1, save_plot_path='plots/stochastic.png')
+		agent.learn(num_seed_runs = args.num_seed_runs)
